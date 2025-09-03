7e3de0cd12177c0200fe621bbd40993d4deb9c65
JValbjorn <julievalbjorn@live.dk>
2025-09-03 11:15:17 +0200

    first comit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Our Website</h1>
+      <p>This is the home page. Explore our site to learn more about us!</p>
+    </div>
+  );
+}
---
+++ src/pages/Projects.jsx
@@ -0,0 +1,7 @@
+export default function Projects() {
+  return (
+    <div>
+      <h1>Our Projects</h1>
+      <p>Here are some of the projects we have worked on recently.</p>
+    </div>
+  );}
