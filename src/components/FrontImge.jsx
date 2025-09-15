b9bdd21758cea5fd78aa1660256b3ee0642ae214
JValbjorn <julievalbjorn@live.dk>
2025-09-15 14:17:07 +0200

    set ups

---
+++ src/components/FrontImge.jsx
@@ -0,0 +1,8 @@
+export default function FrontImge() {
+  return (
+    <div>
+        
+      <img src="" alt="" />
+    </div>
+  );
+}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,15 @@ import { NavLink } from "react-router";
 
 export default function Navbar() {
   return (
+    <>
+    <img src="" alt="" />
     <nav>
       <NavLink to="/" end>Home</NavLink>
       <NavLink to="/project">Project</NavLink>
       <NavLink to="/about">About</NavLink>
       <NavLink to="/contact">Contact</NavLink>
     </nav>
+    {/* < LangueSelector /> */}
+    </>
   );
 }
