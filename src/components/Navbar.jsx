b086dc20d398eb5519f0133aa5c73d5d45fdf2c6
JValbjorn <julievalbjorn@live.dk>
2025-09-16 10:35:19 +0200

    setting up an language selector

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,17 @@
 import { NavLink } from "react-router";
+import LanguageSelector from "./LanguageSelector";
 
 export default function Navbar() {
   return (
     <>
-    <img src="" alt="" />
+    <img src="../../public/img/logo/logo-valbjorn.png" alt="" />
     <nav>
       <NavLink to="/" end>Home</NavLink>
       <NavLink to="/project">Project</NavLink>
       <NavLink to="/about">About</NavLink>
       <NavLink to="/contact">Contact</NavLink>
     </nav>
-    {/* < LangueSelector /> */}
+    <LanguageSelector />
     </>
   );
 }
