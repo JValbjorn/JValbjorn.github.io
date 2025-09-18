a3372a09410045c798bab1444468a839a6518260
JValbjorn <julievalbjorn@live.dk>
2025-09-18 23:12:20 +0200

    deleated end - no errors

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,11 +7,11 @@ export default function Navbar() {
 
   return (
     <div className="navigation">
-      <Link to="/" end><img src="../../public/img/logo/logo-valbjorn.png" alt="logo: <Valbjørn>" /></Link>
+      <Link to="/" ><img src="../../public/img/logo/logo-valbjorn.png" alt="logo: <Valbjørn>" /></Link>
       
       <div className="nav-links">
         <nav>
-          <NavLink to="/" end>{t("Home")}</NavLink>
+          <NavLink to="/">{t("Home")}</NavLink>
           <NavLink to="/project">{t("Projects")}</NavLink>
           <NavLink to="/about">{t("AboutMe")}</NavLink>
           <NavLink to="/contact">{t("Contact")}</NavLink>
