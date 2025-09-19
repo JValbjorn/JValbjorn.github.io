cb880589402c66c77e2957891c3fbab7cd0ee9da
JValbjorn <julievalbjorn@live.dk>
2025-09-19 09:44:58 +0200

    changed path of some doodles

--- src/components/HeroCom.jsx
+++ src/components/HeroCom.jsx
@@ -13,21 +13,67 @@ export default function HeroCom({
   return (
     <>
       <Navbar />
-      
+
       <div className="doodles">
-        <div className="doodle-wrapper d1"><img className="doodle" src="../../public/img/doodles/doodles-disco.png" alt="doodle of a disco ball" /></div>
-        <div className="doodle-wrapper d2"><img className="doodle" src="../../public/img/doodles/doodles-pingu.png" alt="doodle of a pinguin" /></div>
-        <div className="doodle-wrapper d3"><img className="doodle" src="../../public/img/doodles/doodles-puzzle.png" alt="doodle of puzzlepiecs" /></div>
-        <div className="doodle-wrapper  d4"><img className="doodle" src="../../public/img/doodles/doodles-shrimp.png" alt="doodle of happy shrimp" /></div>
-        <div className="doodle-wrapper d5"><img className="doodle " src="../../public/img/doodles/doodles-yarn.png" alt="doodle of yarn and crochet hook" /></div>
-        <div className="doodle-wrapper d6"><img className="doodle" src="../../public/img/doodles/doodles-cheers.png" alt="doodle og a wine bottle and two glasses cheering" /></div>
-        <div className="doodle-wrapper d7"><img className="doodle" src="../../public/img/doodles/doodles-hanger.png" alt="doodle of a clothe hanger" /></div>
-        <div className="doodle-wrapper d8"><img className="doodle" src="../../public/img/doodles/doodles-duck.png" alt="doodle of duck in rainboots" /></div>
-        
+        <div className="doodle-wrapper d1">
+          <img
+            className="doodle"
+            src="../../img/doodles/doodles-disco.png"
+            alt="doodle of a disco ball"
+          />
+        </div>
+        <div className="doodle-wrapper d2">
+          <img
+            className="doodle"
+            src="../../img/doodles/doodles-pingu.png"
+            alt="doodle of a pinguin"
+          />
+        </div>
+        <div className="doodle-wrapper d3">
+          <img
+            className="doodle"
+            src="../../img/doodles/doodles-puzzle.png"
+            alt="doodle of puzzlepiecs"
+          />
+        </div>
+        <div className="doodle-wrapper d4">
+          <img
+            className="doodle"
+            src="../../img/doodles/doodles-shrimp.png"
+            alt="doodle of happy shrimp"
+          />
+        </div>
+        <div className="doodle-wrapper d5">
+          <img
+            className="doodle"
+            src="../../img/doodles/doodles-yarn.png"
+            alt="doodle of yarn and crochet hook"
+          />
+        </div>
+        <div className="doodle-wrapper d6">
+          <img
+            className="doodle"
+            src="../../img/doodles/doodles-cheers.png"
+            alt="doodle og a wine bottle and two glasses cheering"
+          />
+        </div>
+        <div className="doodle-wrapper d7">
+          <img
+            className="doodle"
+            src="../../img/doodles/doodles-hanger.png"
+            alt="doodle of a clothe hanger"
+          />
+        </div>
+        <div className="doodle-wrapper d8">
+          <img
+            className="doodle"
+            src="../../img/doodles/doodles-duck.png"
+            alt="doodle of duck in rainboots"
+          />
+        </div>
       </div>
 
       <section className="header-container">
-        
         <FrontImage imageUrl={imageUrl} />
         <article>
           <h1>{headerText}</h1>
